f90dcd13ae22eabd8a6c6b4490783298573999b5
TSDilov <cv.dilov@abv.bg>
2022-03-30 13:45:27 +0300

    Added Interface of the game

---
+++ ITetrisGame.cs
@@ -0,0 +1,21 @@
+﻿namespace MyPersonalTetris
+{
+    public interface ITetrisGame
+    {
+        Tetromino CurrentFigure { get; set; }
+        int CurrentFigureCol { get; set; }
+        int CurrentFigureRow { get; set; }
+        int Level { get; }
+        int TetrisColumns { get; }
+        bool[,] TetrisField { get; }
+        int TetrisRows { get; }
+
+        void AddCurrentFigureTotetrisField();
+        bool CanMoveToLeft();
+        bool CanMoveToRight();
+        int CheckForFullLines();
+        bool Collision(Tetromino figure);
+        void NewRandomFigure();
+        void UpdateLevel(int score);
+    }
+}
